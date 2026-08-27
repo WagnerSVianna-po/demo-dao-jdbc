2cf597f807ba90a99f10848293bedcfe9c226b69
Wagner <wagnervianna189@gmail.com>
2026-08-27 19:10:26 -0300

    Implementado funções update e findByID em DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,6 +14,19 @@ public class Program2 {
 		Department dep = new Department(null, "Food");
 		depDao.insert(dep);
 		System.out.println("Insert! New department id = " + dep.getId());
+		
+		System.out.println("\n=== TEST 2: department Update ===");
+		Department department = new Department();
+		department = depDao.findById(1);
+		department.setName("Note");
+		depDao.update(department);
+		System.out.println("Update! completed");
+		
+		System.out.println("\n=== TEST 3: department findById ===");
+		department = depDao.findById(1);
+		System.out.println(department);
+		
+		
 
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -56,20 +56,65 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+ "SET "
+					+ "Name = ? "
+					+ "WHERE Id = ?");
+
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+			
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub?
 		
+
 	}
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT * FROM department "
+					+ "WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("Id"));
+				dep.setName(rs.getString("Name"));
+				return dep;
+			}
+			return null;
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
